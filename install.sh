1d46b51529db81545443ed0ce46c8318e017f5dc
Simon Twogood <simon.twogood@gmail.com>
2024-03-29 17:27:56 -0600

    Added lazygit to install script - but it only works with Linux x86 for now

--- install.sh
+++ install.sh
@@ -16,3 +16,9 @@ $BINDIR/chezmoi init git@github.com:s2progger/dotfiles.git
 
 echo "[APPLY] apply dotfiles"
 $BINDIR/chezmoi apply
+
+echo "Install lazygit"
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+cp lazygit $BINDIR/lazygit
